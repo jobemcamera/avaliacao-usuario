d3f9fc5696fbbdceda572bee42026dda8b6787ee
jobemcamera <ti.jobecamera@gmail.com>
2022-10-19 21:41:05 -0300

    botao mostrando os inputs do checkbox e textarea

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function pegaValores() {
-    var estrela = document.getElementsById("mensagem").value;
-    
-    alert(estrela);
-}
+const botao = document.getElementById("enviar")
+const conteudoMensagem = document.getElementById("caixa-mensagem")
+const checkEstrela = document.querySelector(".estrela")
 
+botao.addEventListener("click", mostrar)
+
+function mostrar() {
+    if (document.getElementById("one").checked) {
+        document.getElementById("resultado-estrela").innerHTML = "1/5"
+        
+    } else if (document.getElementById("two").checked) {
+        document.getElementById("resultado-estrela").innerHTML = "2/5"
+
+    } else if (document.getElementById("three").checked) {
+        document.getElementById("resultado-estrela").innerHTML = "3/5"
+
+    } else if (document.getElementById("four").checked) {
+        document.getElementById("resultado-estrela").innerHTML = "4/5"
+
+    } else if (document.getElementById("five").checked) {
+        document.getElementById("resultado-estrela").innerHTML = "5/5"
+        
+    } else 
+        alert("Marque uma estrela")
+
+    document.getElementById("resultado-texto").innerHTML = conteudoMensagem.value;
+}
