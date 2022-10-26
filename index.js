e3ae6613e6ae5ee4d34fe0ac4b16210b5281eaf3
jobemcamera <ti.jobecamera@gmail.com>
2022-10-26 19:08:36 -0300

    resolvido problema de mandar ENVIAR sem marcar pelo menos uma estrela

--- index.js
+++ index.js
@@ -7,33 +7,34 @@ botao.addEventListener("click", mostrar)
 
 function mostrar() {
 
+
+    if (document.getElementById("one").checked) {
+        checkEstrela.innerHTML = "Você deu a nota: 1"
+        checkTexto.innerHTML = conteudoMensagem.value;
+        
+    } else if (document.getElementById("two").checked) {
+        checkEstrela.innerHTML = "Você deu a nota: 2"
+        checkTexto.innerHTML = conteudoMensagem.value;
+
+    } else if (document.getElementById("three").checked) {
+        checkEstrela.innerHTML = "Você deu a nota: 3"
+        checkTexto.innerHTML = conteudoMensagem.value;
+
+    } else if (document.getElementById("four").checked) {
+        checkEstrela.innerHTML = "Você deu a nota: 4"
+        checkTexto.innerHTML = conteudoMensagem.value;
+
+    } else if (document.getElementById("five").checked) {
+        checkEstrela.innerHTML = "Você deu a nota: 5"
+        checkTexto.innerHTML = conteudoMensagem.value;
+        
+    } else 
+        return 0;
+    
     document.getElementById("titulo-principal").style.display = 'none';
     document.getElementById("mensagem-principal").style.display = 'none';
     document.getElementById("class-rate").style.display = 'none';
     document.getElementById("caixa-mensagem").style.display = 'none';
     document.getElementById("enviar").style.display = 'none';
-
     document.getElementById("card-resultado").style.display = 'block';
-
-    if (document.getElementById("one").checked) {
-        checkEstrela.innerHTML = "Você deu a nota: 1"
-        
-    } else if (document.getElementById("two").checked) {
-        checkEstrela.innerHTML = "Você deu a nota: 2"
-
-    } else if (document.getElementById("three").checked) {
-        checkEstrela.innerHTML = "Você deu a nota: 3"
-
-    } else if (document.getElementById("four").checked) {
-        checkEstrela.innerHTML = "Você deu a nota: 4"
-
-    } else if (document.getElementById("five").checked) {
-        checkEstrela.innerHTML = "Você deu a nota: 5"
-        
-    } else 
-        alert("Marque uma estrela")
-        /* aqui não consegui impedir do formulário ser enviado sem nenhum check no radio.*/
-
-    checkTexto.innerHTML = conteudoMensagem.value;
- 
 }
